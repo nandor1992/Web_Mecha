4399b33f16ee47b67c95d7c5354468cd4245a613
Nandor Verba <nandor1992@yahoo.com>
2014-03-01 17:19:32 +0200

    Database
    --Iza's database uploaded

---
+++ database.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2014 at 02:13 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers_number`
+--
+
+CREATE TABLE IF NOT EXISTS `answers_number` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_id` int(11) NOT NULL,
+  `u_id` int(11) NOT NULL,
+  `w_id` int(11) NOT NULL,
+  `a_var` varchar(4) NOT NULL,
+  `answer` int(11) NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers_text`
+--
+
+CREATE TABLE IF NOT EXISTS `answers_text` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_id` int(11) NOT NULL,
+  `u_id` int(11) NOT NULL,
+  `w_id` int(11) NOT NULL,
+  `a_var` varchar(4) NOT NULL,
+  `answer` text NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answers_y/n`
+--
+
+CREATE TABLE IF NOT EXISTS `answers_y/n` (
+  `a_id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_id` int(11) NOT NULL,
+  `u_id` int(11) NOT NULL,
+  `w_id` int(11) NOT NULL,
+  `a_var` varchar(4) NOT NULL,
+  `answer` tinyint(1) NOT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE IF NOT EXISTS `countries` (
+  `q_id` int(11) NOT NULL AUTO_INCREMENT,
+  `country` varchar(40) NOT NULL,
+  PRIMARY KEY (`q_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hint`
+--
+
+CREATE TABLE IF NOT EXISTS `hint` (
+  `h_id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `hint` varchar(2000) NOT NULL,
+  PRIMARY KEY (`h_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `q_id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_type` tinyint(1) NOT NULL,
+  `w_type` int(11) NOT NULL,
+  `question` varchar(1000) NOT NULL,
+  PRIMARY KEY (`q_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_text`
+--
+
+CREATE TABLE IF NOT EXISTS `report_text` (
+  `t_id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(2000) NOT NULL,
+  PRIMARY KEY (`t_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `u_id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_type` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 for admin and 1 for regular users',
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `lst_name` varchar(20) NOT NULL,
+  PRIMARY KEY (`u_id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worksheet`
+--
+
+CREATE TABLE IF NOT EXISTS `worksheet` (
+  `w_id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(11) NOT NULL,
+  `w_name` varchar(30) NOT NULL,
+  `w_type` tinyint(1) NOT NULL COMMENT '0 for Structural 1 for CFD',
+  `w_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`w_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
